2612608087989571f8038b874d500d33226e32c2
Alexander F. Rødseth <rodseth@gmail.com>
2023-07-03 15:51:34 +0200

    Update the test script

--- cmd/render/run.sh
+++ cmd/render/run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-go build -mod=vendor && ./render ../../testdata/xyproto.svg output.png
+go build -mod=vendor
+for x in circle empty rainforest_2c_opt rainforest_8c_opt xyproto; do
+  echo -e -n "Rendering $x:\t\t"
+  ./render ../../testdata/$x.svg $x.png || echo FAIL
+done
